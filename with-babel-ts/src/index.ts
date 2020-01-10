8c654e440107c39dcef46749bde3851734960cdf
zxh <zhangxinghai79@gmail.com>
2020-01-11 02:14:53 +0800

    ha

--- with-babel-ts/src/index.ts
+++ with-babel-ts/src/index.ts
@@ -1,9 +1,23 @@
 // import "core-js"
 import { Human } from "./Human.class"
 
+
+class Female extends Human {
+  isFemale: boolean = true
+}
+
 const human = new Human("Singhi")
 human.intro()
+
+const female = new Female("Wf")
+female.intro()
 
 const str = "Singhi"
 const newStr = str.padEnd(10, "A")
 console.log(newStr)
+
+const arr = Array.from({ '1': 1, '2': 2, length: 2 })
+console.log(arr)
+
+const p = new Promise((r, j) => { r('ok') })
+p.then(console.log)
---
+++ with-babel-ts/src/test.ts
@@ -0,0 +1,11 @@
+class Animal {
+  name: string = "Singhi"
+  say() {
+    console.log(`I am ${this.name}`)
+  }
+}
+
+Promise.resolve().then(console.log)
+
+export {
+}
